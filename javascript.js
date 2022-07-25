2130652a39a9a8420f9d66f087356e8be9ecfa85
Wychbane <davemurphy81@hotmail.com>
2022-07-25 12:27:40 +0100

    Complete game function
    Complete game function and output result. Allow the game to loop
    through 5 times.

--- javascript.js
+++ javascript.js
@@ -5,11 +5,11 @@ Create function to get a random value between 3 choices.
 function getComputerChoice () {
     computerchoice = Math.floor(Math.random() * 3);
     if (computerchoice === 0) {
-        return 'Rock';
+        return 'rock';
     } else if (computerchoice === 1) {
-        return 'Paper';
+        return 'paper';
     } else {
-        return 'Scissors';
+        return 'scissors';
     }
 };
 
@@ -20,19 +20,16 @@ compare user input with computer choice.
 annouce winner. 
 */
 
-let playerSelection = prompt('Rock, Paper or Scissors: ').toLowerCase()
 
 function playRound (computer, player) {
-    console.log(computer);
-    console.log(player);
     if (computer === player) {
         return `You tie! You both picked ${computer}`;
     } else if (computer === 'rock' && player === 'scissors') {
-        return `You lose! Rock beats Scissors`;
+        return `You Lose! Rock beats Scissors`;
     } else if (computer === 'paper' && player === 'rock') {
-        return `You lose! Paper beats Rock`;
+        return `You Lose! Paper beats Rock`;
     } else if (computer === 'scissors' && player === 'paper') {
-        return `You lose! Scissors beats Paper`;
+        return `You Lose! Scissors beats Paper`;
     } else if (computer === 'rock' && player === 'paper') {
         return `You Win! Paper beats Rock`;
     } else if (computer === 'scissors' && player === 'rock') {
@@ -42,4 +39,33 @@ function playRound (computer, player) {
     }
 };
 
-console.log(playRound (getComputerChoice().toLowerCase(), playerSelection));
+/*
+Create a function to play 5 rounds
+Create a for loop. 
+Count number of wins of each
+Output the winner at the end
+*/
+
+function game() {
+    let playerWin = 0;
+    let computerWin = 0;
+    for (let i = 0; i < 5; i++) {
+        let playerSelection = prompt('Rock, Paper or Scissors: ').toLowerCase()
+        let result = playRound(getComputerChoice(), playerSelection);
+        console.log(`Game ${i + 1}/5 ${result}`)
+        if ((result.slice(0, 8)) === 'You Win!') {
+            playerWin++
+        } else if (result.slice(0, 8) === 'You Lose') {
+            computerWin++
+        }
+    }
+    if (playerWin === computerWin) {
+        return `It's a Tie.`
+    } else if (playerWin > computerWin) {
+        return `You won the game ${playerWin} : ${computerWin}.`
+    } else {
+        return `You lost the game ${playerWin} : ${computerWin}.`
+    }
+}
+
+console.log(game());
